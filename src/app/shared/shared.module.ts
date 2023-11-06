e5fc1a0432337f6714c715eff52ddcca6c4c5e01
ManuDve <m.alfaro@live.cl>
2023-11-06 17:14:40 -0300

    Genera interfaz basica

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+@NgModule({
+  imports: [],
+  exports: [NavBarComponent],
+  declarations: [NavBarComponent],
+  providers: [],
+})
+export class SharedModule { }
